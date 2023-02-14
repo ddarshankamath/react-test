5a5f1baf30dc52a49a13424d59079430a1bc225e
Darshan D Kamath <ddarshankamath@gmail.com>
2023-02-14 12:53:08 +0530

    firstcommit

---
+++ src/components/Demo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Demo2 from "./Demo2";
+const Demo = (props) => {
+  return (
+    <div>
+      <Demo2 />
+      <h1>abcd</h1>
+      {/* <h2>{props.para}</h2> */}
+    </div>
+  );
+};
+
+export default Demo;
---
+++ src/components/Demo2.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Demo2 = () => {
+  return (
+    <div>
+      <h2>xyz</h2>
+    </div>
+  );
+};
+export default Demo2;
